0c6987e40bea6dd16b776ca570fbc65b5838f551
Patrick <patrickbarriente@gmail.com>
2020-10-04 20:35:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import RecipeHomePageView from './pages/Recipes/Recipes';
 import Login from './pages/User/login';
 import Signup from './pages/User/Signup';
 
-
 function App() {
   return (
     <Router>
@@ -16,7 +15,6 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" component={RecipeHomePageView} exact />
-        <Route path="/share-recipe" component={ShareRecipe} />
         <Route path="/about" component={AboutPageView} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
@@ -26,79 +24,4 @@ function App() {
   );
 }
 
-export {App};
-
-class Recipes extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Recipes</h1>
-      </div>
-    )
-  } 
-}
-
-export {Recipes};
-
-class ShareRecipe extends Component {
-  render(){
-    return (
-      <div>
-        <h1>Share Recipe</h1>
-      </div>
-    )
-  }
-}
-
-export {ShareRecipe};
-
-class MyRecipes extends Component {
-  render() {
-    return (
-      <div>
-        <h1>My Recipes</h1>
-      </div>
-    )
-  }
-}
-
-export {MyRecipes};
-
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <h1>About</h1>
-      </div>
-    )
-  }
-}
-
-export {About};
-
-class LoginSignUp extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Login / Sign Up</h1>
-      </div>
-    )
-  }
-}
-
-export {LoginSignUp};
-
-ReactDOM.render((
-  <Router history = {browserHistory}>
-    <Route path = "/" component = {App}>
-      <IndexRout component = {Recipes} />
-      <Route path = "recipes" component = {Recipes} />
-      <Route path = "share-recipe" component = {ShareRecipe} />
-      <Route path = "my-recipes" component = {MyRecipes} />
-      <Route path = "about" component = {About} />
-      <Route path = "loginsignup" component = {LoginSignup} />
-     </Route>
-  </Router>
-), document.getElementById('app'))
-
-export default App
+export default App;
