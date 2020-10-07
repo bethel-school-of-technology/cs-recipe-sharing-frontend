7bbe9cf9dc5c6768ed9c5ce4aff06f419e4e54fb
Ethan McNeil <mcneil.ethan@gmail.com>
2020-10-07 15:36:33 -0400

    ""

--- src/pages/Share-A-Recipe/Share-Recipe.js
+++ src/pages/Share-A-Recipe/Share-Recipe.js
@@ -7,15 +7,12 @@ import AuthService from '../../services/auth.service';
 let currentUser = AuthService.getCurrentUser();
 console.log("User Value: ", currentUser);
 
-<<<<<<< HEAD
 const ShareRecipe = withRouter(({ history }) => {
-=======
     let currentUser = AuthService.getCurrentUser();
 
     let headers = {
         authorization: currentUser.authorization
     }
->>>>>>> 771acf3824d4affba6986319c39776f56c5f6d08
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
@@ -28,35 +25,6 @@ const ShareRecipe = withRouter(({ history }) => {
     }]);
     const [difficulty, setDifficulty] = useState("Medium");
     const [directions, setDirections] = useState("");
-<<<<<<< HEAD
-    const [author, setAuthor] = useState("");
-
-    const [selectedFile, setState] = useState([]);
-
-    const url = "http://localhost:8080";
-
-    let headers = {
-        authorization: currentUser.authorization
-    }
-
-    const fileChangedHandler = (event) => {
-        setState({selectedFile: event.target.files[0]})
-        console.log(selectedFile);
-        }
-    const uploadHandler = async (e) => {
-        e.preventDefault(); 
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-        console.log(formData, selectedFile);
-        let response = await axios.post(`${url}/api/recipe/image`, formData, {headers: headers});
-        if(response.status === 200){
-            window.alert(response.statusText);
-        }
-        else{
-            window.alert("Something went wrong." + response.statusText);
-        }
-     }
-=======
     
 
     const url = "http://localhost:8080";
@@ -77,7 +45,6 @@ const ShareRecipe = withRouter(({ history }) => {
             console.log(response);
         }
     }
->>>>>>> 771acf3824d4affba6986319c39776f56c5f6d08
 
 
     const handleSubmit = async (e) => {
@@ -92,11 +59,7 @@ const ShareRecipe = withRouter(({ history }) => {
            image: document.getElementById("imagePreview").src,
            directions: directions,
            author: currentUser.user,
-<<<<<<< HEAD
-           authorId: 3
-=======
            authorId: currentUser.id
->>>>>>> 771acf3824d4affba6986319c39776f56c5f6d08
         }
         //need to format the ingredients
         let response = await axios.post(`${url}/api/recipe/add/`, recipe, {headers: headers});
@@ -116,15 +79,6 @@ const ShareRecipe = withRouter(({ history }) => {
     return (
         <div className="page-container">
                 <div className="title">Share Your Favorite Recipe</div>
-<<<<<<< HEAD
-
-                <form>
-                     <div>
-                      <input type="file" onChange={fileChangedHandler}/>
-                          <button onClick={uploadHandler}>Upload!</button>
-                    </div>
-                </form>
-=======
                 <form onSubmit={uploadPicture} >
                     <div className="fileUpload">
                             <label>Upload a picture for your recipe!</label>
@@ -133,7 +87,6 @@ const ShareRecipe = withRouter(({ history }) => {
 
                      </div>
                  </form>
->>>>>>> 771acf3824d4affba6986319c39776f56c5f6d08
                 <form onSubmit={handleSubmit}>
                     <div className="recipeFormContainer">
                         <div className="row1">
@@ -153,14 +106,8 @@ const ShareRecipe = withRouter(({ history }) => {
                                 <option value="Difficult">Difficult</option>
                             </select>                   
                             </div>
-<<<<<<< HEAD
-                            <div className="fileUpload">
-                               <label>Upload a picture for your recipe!</label>
-                            <input type="file"/>
-=======
                             <div className="displayImage">
                                 <img id="imagePreview" src={require(`../../assets/images/bacon-squash.jpg`)} />
->>>>>>> 771acf3824d4affba6986319c39776f56c5f6d08
                             </div>
                             </div>
                             <div className="row2">
